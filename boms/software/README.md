d4c1662a406d78b08b60b43d15d18cf2d7126fd6
Github pipeline <pipeline@github.com>
2022-04-11 22:08:46 -0500

    Add script to upload BOMs for Software

--- boms/software/README.md
+++ boms/software/README.md
@@ -6,4 +6,16 @@ The software automation is provided in the following folders:
 - [Integration](integration) - IBM Intergration Platform (APIC, ACE, MQ, EventStreams, etc)
 - [Maximo](maximo) - Maximo Application Suite
 - [Telco Cloud](telco-cloud) - Telco Industry cloud
+- [Dev Tools](devtools) - Cloud-Native Toolkit
+
+### Loading Files into Ascent
+
+To auto load all the BOMs into the Ascent tool run the following command
+
+```bash
+./load-ascent.sh <ASCENT_API_TOKEN> <URL TO API>
+```
+
+This will auto ingest the Software Bill of Materials into the Ascent Tool
+
 
---
+++ boms/software/load-ascent.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Script for loading BOMs into Ascent
+# "https://<deployed-cluster-url>/architectures/boms/import?overwrite=true&public=true"
+
+TOKEN="$1"
+URL="$2"
+
+if [[ -z "${TOKEN}" && "${URL}"  ]]; then
+  echo "usage: load-ascent.sh ASCENT_TOKEN TARGET_URL"
+  exit 1
+fi
+
+TOKEN="Authorization: Bearer ${TOKEN}"
+CURDIR=$(pwd)
+
+files="/datafabric/600-datafabric-multicloud.yaml \
+/devtools/220-dev-tools.yaml \
+/gitops/200-gitops-bootstrap.yaml \
+/gitops/200-openshift-gitops-bootstrap.yaml \
+/integration/200-openshift-gitops.yaml \
+/integration/300-integration-platform-multicloud.yaml \
+/maximo/200-openshift-gitops.yaml \
+/maximo/400-mas-core-multicloud.yaml \
+/telco-cloud/500-telco-all.yaml \
+/telco-cloud/500-telco-core.yaml \
+/telco-cloud/500-telco-data.yaml \
+/telco-cloud/500-telco-integration.yaml \
+/turbonomic/200-openshift-gitops.yaml \
+/turbonomic/202-ibmcloud-storage-class.yaml \
+/turbonomic/250-turbonomic-multicloud.yaml"
+
+for loadfile in ${files[@]}; do
+  echo $str
+
+    file='=@"'${CURDIR}$loadfile'"'
+    echo $file
+    curl --location --request POST ${URL} \
+      --header "${TOKEN}"  --form "${file}"
+    echo "\n"
+
+done
+
