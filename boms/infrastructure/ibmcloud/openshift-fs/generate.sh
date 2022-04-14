0b93d5245a9b4a8142332f68e87d346f4cfb1fdb
Andrew Trice <amtrice@us.ibm.com>
2022-04-14 11:35:24 -0400

    fs updates
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

---
+++ boms/infrastructure/ibmcloud/openshift-fs/generate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# IBM Ecosystem Lab Team
+# Install iascable and run this script to produce an target public source repository
+
+GENERATE_TARGET="$1"
+
+if [ -z "$GENERATE_TARGET" ]
+then
+      GENERATE_TARGET="all"
+fi
+
+mkdir -p ../../../../automation-fscloud
+
+case "$GENERATE_TARGET" in
+  "all" | "infra" | "infrastructure" | "i")
+    iascable build -i ./000-ibm-fs-account-setup.yaml -o ../../../../automation-fscloud
+    iascable build -i ./100-ibm-fs-shared-services.yaml -o ../../../../automation-fscloud
+    iascable build -i ./110-ibm-fs-edge-vpc.yaml -o ../../../../automation-fscloud
+    iascable build -i ./120-ibm-fs-management-vpc.yaml -o ../../../../automation-fscloud
+    iascable build -i ./130-ibm-fs-management-vpc-openshift.yaml -o ../../../../automation-fscloud
+    iascable build -i ./140-ibm-fs-workload-vpc.yaml -o ../../../../automation-fscloud
+    iascable build -i ./150-ibm-fs-workload-vpc-openshift.yaml -o ../../../../automation-fscloud
+  ;;
+esac
+
+
+case "$GENERATE_TARGET" in
+  "all" | "software" | "s")
+    iascable build -i ./160-ibm-fs-openshift-dev-tools.yaml -o ../../../../automation-fscloud
+    iascable build -i ./165-ibm-fs-openshift-workload.yaml -o ../../../../automation-fscloud
+    iascable build -i ./170-ibm-fs-openshift-gitops.yaml -o ../../../../automation-fscloud
+  ;;
+esac
+
+cp -R ./files/* ../../../../automation-fscloud
