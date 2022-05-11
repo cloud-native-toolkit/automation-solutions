8675e3f0178503169cfd6eaf808164c743fde6a4
Sean Sundberg <seansund@us.ibm.com>
2022-05-10 23:42:06 -0500

    Updates fscloud supporting files from automated testing
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- boms/infrastructure/ibmcloud/openshift-fs/generate.sh
+++ boms/infrastructure/ibmcloud/openshift-fs/generate.sh
@@ -4,33 +4,39 @@
 # Install iascable and run this script to produce an target public source repository
 
 GENERATE_TARGET="$1"
+GENERATE_DESTINATION="$2"
 
-if [ -z "$GENERATE_TARGET" ]
+if [[ -z "$GENERATE_TARGET" ]]
 then
       GENERATE_TARGET="all"
 fi
 
-mkdir -p ../../../../automation-fscloud
+if [[ -z "${GENERATE_DESTINATION}" ]]; then
+  GENERATE_DESTINATION="../../../../automation-fscloud"
+fi
+
+mkdir -p "${GENERATE_DESTINATION}"
 
 case "$GENERATE_TARGET" in
   "all" | "infra" | "infrastructure" | "i")
-    iascable build -i ./000-ibm-fs-account-setup.yaml -o ../../../../../automation-fscloud
-    iascable build -i ./100-ibm-fs-shared-services.yaml -o ../../../../../automation-fscloud
-    iascable build -i ./110-ibm-fs-edge-vpc.yaml -o ../../../../../automation-fscloud
-    iascable build -i ./120-ibm-fs-management-vpc.yaml -o ../../../../../automation-fscloud
-    iascable build -i ./130-ibm-fs-management-vpc-openshift.yaml -o ../../../../../automation-fscloud
-    iascable build -i ./140-ibm-fs-workload-vpc.yaml -o ../../../../../automation-fscloud
-    iascable build -i ./150-ibm-fs-workload-vpc-openshift.yaml -o ../../../../../automation-fscloud
+    iascable build -i ./000-ibm-fs-account-setup.yaml -o "${GENERATE_DESTINATION}"
+    iascable build -i ./100-ibm-fs-shared-services.yaml -o "${GENERATE_DESTINATION}"
+    iascable build -i ./110-ibm-fs-edge-vpc.yaml -o "${GENERATE_DESTINATION}"
+    iascable build -i ./120-ibm-fs-management-vpc.yaml -o "${GENERATE_DESTINATION}"
+    iascable build -i ./130-ibm-fs-management-vpc-openshift.yaml -o "${GENERATE_DESTINATION}"
+    iascable build -i ./140-ibm-fs-workload-vpc.yaml -o "${GENERATE_DESTINATION}"
+    iascable build -i ./150-ibm-fs-workload-vpc-openshift.yaml -o "${GENERATE_DESTINATION}"
   ;;
 esac
 
 
 case "$GENERATE_TARGET" in
   "all" | "software" | "s")
-    iascable build -i ./160-ibm-fs-openshift-dev-tools.yaml -o ../../../../../automation-fscloud
-    iascable build -i ./165-ibm-fs-openshift-workload.yaml -o ../../../../../automation-fscloud
-    iascable build -i ./170-ibm-fs-openshift-gitops.yaml -o ../../../../../automation-fscloud
+    iascable build -i ./160-ibm-fs-openshift-dev-tools.yaml -o "${GENERATE_DESTINATION}"
+    iascable build -i ./165-ibm-fs-openshift-workload.yaml -o "${GENERATE_DESTINATION}"
+    iascable build -i ./170-ibm-fs-openshift-gitops.yaml -o "${GENERATE_DESTINATION}"
   ;;
 esac
 
-cp -R ./files/* ../../../../../automation-fscloud
+cp -R ./files/* "${GENERATE_DESTINATION}"
+
