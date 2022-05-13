b7f4d449003c0b0a2de62aee5e9c31105a813b75
Sean Sundberg <seansund@us.ibm.com>
2022-05-13 13:18:30 -0500

    Updates generate scripts with optimization from iascable
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- boms/infrastructure/ibmcloud/openshift-fs/generate.sh
+++ boms/infrastructure/ibmcloud/openshift-fs/generate.sh
@@ -6,6 +6,21 @@
 GENERATE_TARGET="$1"
 GENERATE_DESTINATION="$2"
 
+if ! command -v iascable 1> /dev/null 2> /dev/null; then
+  echo "iascable cli not found" >&2
+  echo "  Install iascable with this command: curl -sL https://raw.githubusercontent.com/cloud-native-toolkit/iascable/main/install.sh | sh" >&2
+  exit 1
+fi
+
+IASCABLE_MAJOR_VERSION=$(iascable --version | sed -E "s/^([0-9]+)[.][0-9]+[.][0-9]+/\1/g")
+IASCABLE_MINOR_VERSION=$(iascable --version | sed -E "s/^[0-9]+[.]([0-9]+)[.][0-9]+/\1/g")
+
+if [[ "${IASCABLE_MAJOR_VERSION}" -le 2 ]] && [[ "${IASCABLE_MINOR_VERSION}" -le 11 ]]; then
+  echo "Installed iascable cli is backlevel version"
+  echo "  Update iascable with this command: curl -sL https://raw.githubusercontent.com/cloud-native-toolkit/iascable/main/install.sh | sh" >&2
+  exit 1
+fi
+
 if [[ -z "$GENERATE_TARGET" ]]
 then
       GENERATE_TARGET="all"
@@ -19,22 +34,26 @@ mkdir -p "${GENERATE_DESTINATION}"
 
 case "$GENERATE_TARGET" in
   "all" | "infra" | "infrastructure" | "i")
-    iascable build -i ./000-ibm-fs-account-setup.yaml -o "${GENERATE_DESTINATION}"
-    iascable build -i ./100-ibm-fs-shared-services.yaml -o "${GENERATE_DESTINATION}"
-    iascable build -i ./110-ibm-fs-edge-vpc.yaml -o "${GENERATE_DESTINATION}"
-    iascable build -i ./120-ibm-fs-management-vpc.yaml -o "${GENERATE_DESTINATION}"
-    iascable build -i ./130-ibm-fs-management-vpc-openshift.yaml -o "${GENERATE_DESTINATION}"
-    iascable build -i ./140-ibm-fs-workload-vpc.yaml -o "${GENERATE_DESTINATION}"
-    iascable build -i ./150-ibm-fs-workload-vpc-openshift.yaml -o "${GENERATE_DESTINATION}"
+    iascable build \
+      -i ./000-ibm-fs-account-setup.yaml \
+      -i ./100-ibm-fs-shared-services.yaml \
+      -i ./110-ibm-fs-edge-vpc.yaml \
+      -i ./120-ibm-fs-management-vpc.yaml \
+      -i ./130-ibm-fs-management-vpc-openshift.yaml \
+      -i ./140-ibm-fs-workload-vpc.yaml \
+      -i ./150-ibm-fs-workload-vpc-openshift.yaml \
+      -o "${GENERATE_DESTINATION}"
   ;;
 esac
 
 
 case "$GENERATE_TARGET" in
   "all" | "software" | "s")
-    iascable build -i ./160-ibm-fs-openshift-dev-tools.yaml -o "${GENERATE_DESTINATION}"
-    iascable build -i ./165-ibm-fs-openshift-workload.yaml -o "${GENERATE_DESTINATION}"
-    iascable build -i ./170-ibm-fs-openshift-gitops.yaml -o "${GENERATE_DESTINATION}"
+    iascable build \
+      -i ./160-ibm-fs-openshift-dev-tools.yaml \
+      -i ./165-ibm-fs-openshift-workload.yaml \
+      -i ./170-ibm-fs-openshift-gitops.yaml \
+      -o "${GENERATE_DESTINATION}"
   ;;
 esac
 
