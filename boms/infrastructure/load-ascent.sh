ee62b8900adb880c10aabc390bdda30d3a35fc2e
Github pipeline <pipeline@github.com>
2022-06-04 16:33:37 -0500

    upload load-ascent scripts

--- boms/infrastructure/load-ascent.sh
+++ boms/infrastructure/load-ascent.sh
@@ -4,9 +4,9 @@
 # "https://<deployed-cluster-url>/architectures/boms/import?overwrite=true&public=true"
 
 TOKEN="$1"
-URL="$2"
+URL="https://ascent-bff-mapper-staging.dev-mapper-ocp-4be51d31de4db1e93b5ed298cdf6bb62-0000.eu-de.containers.appdomain.cloud"
 
-if [[ -z "${TOKEN}" && "${URL}"  ]]; then
+if [[ -z "${TOKEN}" && -z "${URL}"  ]]; then
   echo "usage: load-ascent.sh ASCENT_TOKEN TARGET_URL"
   exit 1
 fi
@@ -24,6 +24,8 @@ files="/aws/_common/gitops/200-aws-openshift-gitops.yaml \
 /ibmcloud/_common/gitops/200-ibm-openshift-gitops.yaml \
 /ibmcloud/_common/storage/210-ibm-odf-storage.yaml \
 /ibmcloud/_common/storage/210-ibm-portworx-storage.yaml \
+/ibmcloud/openshift/1-quickstart/105-ibm-vpc-openshift.yaml \
+/ibmcloud/openshift/1-quickstart/200-ibm-openshift-gitops.yaml \
 /ibmcloud/openshift/1-quickstart/105-ibm-vpc-openshift.yaml \
 /ibmcloud/openshift/2-standard/000-ibm-account-setup.yaml \
 /ibmcloud/openshift/2-standard/100-ibm-shared-services.yaml \
@@ -49,14 +51,20 @@ files="/aws/_common/gitops/200-aws-openshift-gitops.yaml \
 /ibmcloud/openshift-fs/150-ibm-fs-workload-vpc-openshift.yaml \
 /ibmcloud/openshift-fs/160-ibm-fs-openshift-dev-tools.yaml \
 /ibmcloud/openshift-fs/165-ibm-fs-openshift-workload.yaml \
-/ibmcloud/openshift-fs/170-ibm-fs-openshift-gitops.yaml "
+/ibmcloud/openshift-fs/170-ibm-fs-openshift-gitops.yaml  \
+/ibmcloud/zos-dev/000-ibm-zdev-account-setup.yaml \
+/ibmcloud/zos-dev/100-ibm-zdev-shared-services.yaml \
+/ibmcloud/zos-dev/110-ibm-zdev-network-vpc.yaml \
+/ibmcloud/zos-dev/120-ibm-zdev-development-vpc.yaml \
+/ibmcloud/zos-dev/130-ibm-zdev-development-vpc-openshift.yaml \
+/ibmcloud/zos-dev/160-ibm-zdev-openshift-dev-tools.yaml "
 
 for loadfile in ${files[@]}; do
   echo $str
 
     file='=@"'${CURDIR}$loadfile'"'
     echo $file
-    curl --location --request POST ${URL} \
+    curl --location --request POST "${URL}/architectures/boms/import?overwrite=true&public=true" \
       --header "${TOKEN}"  --form "${file}"
     echo "\n"
 
