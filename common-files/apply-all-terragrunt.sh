b445f582680c96d9daf7ca91bd9eb36489ddde85
Sean Sundberg <seansund@us.ibm.com>
2022-07-05 16:21:05 -0500

    Updates apply-all and destroy-all terragrunt scripts
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- common-files/apply-all-terragrunt.sh
+++ common-files/apply-all-terragrunt.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "y" | terragrunt run-all apply || exit 1
+echo "y" | terragrunt run-all apply --terragrunt-parallelism 1 || exit 1
--- common-files/destroy-all-terragrunt.sh
+++ common-files/destroy-all-terragrunt.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "y" | terragrunt run-all destroy || exit 1
+echo "y" | terragrunt run-all destroy --terragrunt-parallelism 1 || exit 1
