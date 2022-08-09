51eaa45a6f1307026a12b9b42502d31ec7efc766
Sean Sundberg <seansund@us.ibm.com>
2022-08-08 22:47:13 -0500

    Updates generate script logic
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- boms/software/turbonomic/generate.sh
+++ boms/software/turbonomic/generate.sh
@@ -3,9 +3,38 @@
 # IBM Ecosystem Lab Team
 # Install iascable and run this script to produce an target public source repository
 
-iascable build -i ./200-openshift-gitops.yaml -o ../../../../automation-turbonomic
-iascable build -i ./250-turbonomic-multicloud.yaml -o ../../../../automation-turbonomic
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+
+TARGET_BASE="${1:-../../../..}"
+TARGET_REPO="${2:-automation-turbonomic}"
+
+TARGET_DIR="${TARGET_BASE}/${TARGET_REPO}"
+
+if ! command -v iascable 1> /dev/null 2> /dev/null; then
+  echo "iascable cli not found" >&2
+  echo "  Install iascable with this command: curl -sL https://raw.githubusercontent.com/cloud-native-toolkit/iascable/main/install.sh | sh" >&2
+  exit 1
+fi
+
+IASCABLE_MAJOR_VERSION=$(iascable --version | sed -E "s/^([0-9]+)[.][0-9]+[.][0-9]+/\1/g")
+IASCABLE_MINOR_VERSION=$(iascable --version | sed -E "s/^[0-9]+[.]([0-9]+)[.][0-9]+/\1/g")
+
+if [[ "${IASCABLE_MAJOR_VERSION}" -le 2 ]] && [[ "${IASCABLE_MINOR_VERSION}" -lt 13 ]]; then
+  echo "Installed iascable cli is backlevel version"
+  echo "  Update iascable with this command: curl -sL https://raw.githubusercontent.com/cloud-native-toolkit/iascable/main/install.sh | sh" >&2
+  exit 1
+fi
+
+echo "Generating output into ${TARGET_DIR}"
+
+iascable build \
+  -i ./200-openshift-gitops.yaml \
+  -i ./250-turbonomic-multicloud.yaml \
+  -o "${TARGET_DIR}" \
+  --flatten
+
 echo "Copying Files"
-cp ./files/* ../../../../automation-turbonomic
+cp -R -L ./files/* "${TARGET_DIR}"
+
 echo "Copying Configuration"
-cp -R -L ./files/configuration ../../../../automation-turbonomic/configuration
+cp -R -L ./files/configuration "${TARGET_DIR}"
