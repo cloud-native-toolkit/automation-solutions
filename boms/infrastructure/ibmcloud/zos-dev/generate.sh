53b4ed7e667b8aa043a2f4331fde4f0bfc89f6eb
Andrew Trice <amtrice@us.ibm.com>
2022-10-18 23:54:30 -0400

    work in progress: zos-dev

--- boms/infrastructure/ibmcloud/zos-dev/generate.sh
+++ boms/infrastructure/ibmcloud/zos-dev/generate.sh
@@ -4,6 +4,7 @@
 # Install iascable and run this script to produce an target public source repository
 
 GENERATE_TARGET="$1"
+GENERATE_DESTINATION="../../../../../automation-ibmcloud-infra-zos-dev"
 
 if [ -z "$GENERATE_TARGET" ]
 then
@@ -12,19 +13,27 @@ fi
 
 case "$GENERATE_TARGET" in
   "all" | "infra" | "infrastructure" | "i")
-    iascable build -i ./000-ibm-zdev-account-setup.yaml -o ../../../../../automation-ibmcloud-infra-zos-dev
-    iascable build -i ./100-ibm-zdev-shared-services.yaml -o ../../../../../automation-ibmcloud-infra-zos-dev
-    iascable build -i ./110-ibm-zdev-network-vpc.yaml -o ../../../../../automation-ibmcloud-infra-zos-dev
-    iascable build -i ./120-ibm-zdev-development-vpc.yaml -o ../../../../../automation-ibmcloud-infra-zos-dev
-    iascable build -i ./130-ibm-zdev-development-vpc-openshift.yaml -o ../../../../../automation-ibmcloud-infra-zos-dev
+    iascable build -i ./000-ibm-zdev-account-setup.yaml -o $GENERATE_DESTINATION
+    iascable build -i ./100-ibm-zdev-shared-services.yaml -o $GENERATE_DESTINATION
+    iascable build -i ./110-ibm-zdev-network-vpc.yaml -o $GENERATE_DESTINATION
+    iascable build -i ./120-ibm-zdev-network-vpc-openshift.yaml -o $GENERATE_DESTINATION
   ;;
 esac
 
 case "$GENERATE_TARGET" in
   "all" | "software" | "s")
-    iascable build -i ./160-ibm-zdev-openshift-dev-tools.yaml -o ../../../../../automation-ibmcloud-infra-zos-dev
+    iascable build -i ./160-ibm-zdev-openshift-dev-tools.yaml -o $GENERATE_DESTINATION
   ;;
 esac
 
-cp -v -R -L ./files/* ../../../../../automation-ibmcloud-infra-zos-dev
+cp -v -R -L ./files/* $GENERATE_DESTINATION
+
+
+# remove the default, aws, and azure docker images, and uncomment the ibm image
+sed -i '' 's/DOCKER_IMAGE\="quay.io\/cloudnativetoolkit\/cli-tools\:.*//' ${GENERATE_DESTINATION}/launch.sh
+sed -i '' 's/#AWS.*//' ${GENERATE_DESTINATION}/launch.sh
+sed -i '' 's/#AZURE.*//' ${GENERATE_DESTINATION}/launch.sh
+sed -i '' 's/#IBM //' ${GENERATE_DESTINATION}/launch.sh
+cat ${GENERATE_DESTINATION}/launch.sh | grep quay
+
 
